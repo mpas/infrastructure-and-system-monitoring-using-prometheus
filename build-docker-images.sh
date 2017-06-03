79ef9b046ddfa769f527d7c51607c296aaff4d1a
marco.pas@philips.com <marco.pas@philips.com>
2017-06-03 12:09:18 +0200

    Text correction

--- build-docker-images.sh
+++ build-docker-images.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 echo ""
-echo "Building SpringBoot Prometheus Demo image"
+echo "Building Spring Boot Prometheus Demo image"
 echo "--------------------------------------------------------------------------------"
 cd ./docker-images/spring-boot-prometheus-demo
 bash build.sh
